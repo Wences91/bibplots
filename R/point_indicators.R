f64d804470a819737642acb1d09cd419a2d3db13
Wences91 <wences91@gmail.com>
2020-02-20 18:01:42 +0100

    Update point_indicators.R

--- R/point_indicators.R
+++ R/point_indicators.R
@@ -4,15 +4,15 @@
 #' @param size size of value labels
 #' @param vjust vertical justification of value labels
 #' @export
-#' @importFrom ggplot2 ggplot geom_point geom_text labs scale_y_discrete scale_color_gradient theme_minimal
+#' @importFrom ggplot2 ggplot aes geom_point geom_text labs scale_y_discrete scale_color_gradient theme_minimal
 #' 
 
 point_indicators <- function(indicators, size=1, vjust=0.5){
   p <- ggplot2::ggplot()
   
   sapply(unique(indicators[,2]), function(x){
-    p <<- p + ggplot2::geom_point(data=indicators[which(indicators[,2]==x),], aes(x=indicators[which(indicators[,2]==x),1], y=indicators[which(indicators[,2]==x),2], color=100*indicators[which(indicators[,2]==x),3]/sum(indicators[which(indicators[,2]==x),3])), size=100*indicators[which(indicators[,2]==x),3]/sum(indicators[which(indicators[,2]==x),3]), alpha=.8)
-    p <<- p + ggplot2::geom_text(data=indicators[which(indicators[,2]==x),], aes(x=indicators[which(indicators[,2]==x),1], y=indicators[which(indicators[,2]==x),2], label=as.character(indicators[which(indicators[,2]==x),3])), size=size, vjust=vjust, color='grey30')
+    p <<- p + ggplot2::geom_point(data=indicators[which(indicators[,2]==x),], ggplot2::aes(x=indicators[which(indicators[,2]==x),1], y=indicators[which(indicators[,2]==x),2], color=100*indicators[which(indicators[,2]==x),3]/sum(indicators[which(indicators[,2]==x),3])), size=100*indicators[which(indicators[,2]==x),3]/sum(indicators[which(indicators[,2]==x),3]), alpha=.8)
+    p <<- p + ggplot2::geom_text(data=indicators[which(indicators[,2]==x),], ggplot2::aes(x=indicators[which(indicators[,2]==x),1], y=indicators[which(indicators[,2]==x),2], label=as.character(indicators[which(indicators[,2]==x),3])), size=size, vjust=vjust, color='grey30')
   })
   
   p <- p + ggplot2::labs(y='', x='') +
