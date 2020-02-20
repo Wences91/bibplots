afe56cf98620d0920abae1c6f75314e217c16d73
Wences91 <wences91@gmail.com>
2020-02-20 20:59:01 +0100

    Update point_indicators.R

--- R/point_indicators.R
+++ R/point_indicators.R
@@ -1,6 +1,6 @@
 #' point_indicators
 #' 
-#' @param indicators data.frame whith thre variables: 1 actors, 2 indicators and 3 values
+#' @param indicators indicators data.frame
 #' @param size size of value labels
 #' @param vjust vertical justification of value labels
 #' @export
@@ -10,13 +10,14 @@
 point_indicators <- function(indicators, size=1, vjust=0.5){
   p <- ggplot2::ggplot()
   
-  sapply(unique(indicators[,2]), function(x){
-    p <<- p + ggplot2::geom_point(data=indicators[which(indicators[,2]==x),], ggplot2::aes(x=indicators[which(indicators[,2]==x),1], y=indicators[which(indicators[,2]==x),2], color=100*indicators[which(indicators[,2]==x),3]/sum(indicators[which(indicators[,2]==x),3])), size=100*indicators[which(indicators[,2]==x),3]/sum(indicators[which(indicators[,2]==x),3]), alpha=.8)
-    p <<- p + ggplot2::geom_text(data=indicators[which(indicators[,2]==x),], ggplot2::aes(x=indicators[which(indicators[,2]==x),1], y=indicators[which(indicators[,2]==x),2], label=as.character(indicators[which(indicators[,2]==x),3])), size=size, vjust=vjust, color='grey30')
+  sapply(unique(names(indicators)), function(x){
+    print(x)
+    p <<- p + ggplot2::geom_point(ggplot2::aes(x=row.names(indicators), y=x, color=100*indicators[,x]/sum(indicators[,x])), size=100*indicators[,x]/sum(indicators[,x]), alpha=.8)
+    p <<- p + ggplot2::geom_text(ggplot2::aes(x=row.names(indicators), y=x, label=as.character(indicators[,x])), size=size, vjust=vjust, color='grey30')
   })
   
   p <- p + ggplot2::labs(y='', x='') +
-    ggplot2::scale_y_discrete(limits = rev(sort(unique(indicators[,2])))) +
+    ggplot2::scale_y_discrete(limits = rev(sort(unique(names(indicators))))) +
     ggplot2::scale_color_gradient('Legend', low='cornsilk', high='red', label=function(x) paste0(x,'%')) +
     ggplot2::theme_minimal()
   
